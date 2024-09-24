9c65ff4c46b32d62e5249f6aaedb284b46f915e4
geremyLozano <157999676+geremyLozano@users.noreply.github.com>
2024-09-24 17:27:17 -0500

    Update AdministradorDao.java

--- Nueva carpeta/SoftCapsuleCareController/src/pe/edu/pucp/citamedica/dao/AdministradorDao.java
+++ Nueva carpeta/SoftCapsuleCareController/src/pe/edu/pucp/citamedica/dao/AdministradorDao.java
@@ -3,7 +3,7 @@ package pe.edu.pucp.citamedica.dao;
 import java.util.ArrayList;
 import pe.edu.pucp.citamedica.clinica.model.Administrador;
 
-public interface AdministradorDAO {
+public interface AdministradorDao {
     
     int insertar(Administrador administrador);
     int modificar(Administrador administrador);
