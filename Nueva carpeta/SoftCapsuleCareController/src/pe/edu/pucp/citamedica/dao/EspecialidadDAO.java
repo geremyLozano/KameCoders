e52175457d71c92f7b88f266b7149db079a1fbd5
FrankDarkShadow <gianfranco.mpc@gmail.com>
2024-09-24 18:13:26 -0500

    cambio
    cambio

--- Nueva carpeta/SoftCapsuleCareController/src/pe/edu/pucp/citamedica/dao/EspecialidadDAO.java
+++ Nueva carpeta/SoftCapsuleCareController/src/pe/edu/pucp/citamedica/dao/EspecialidadDAO.java
@@ -3,7 +3,7 @@ package pe.edu.pucp.citamedica.dao;
 import java.util.ArrayList;
 import pe.edu.pucp.citamedica.clinica.model.Especialidad;
 
-public Interface EspecialidadDAO{
+public interface EspecialidadDAO{
     int insertar(Especialidad especialidad);
     int modificar(Especialidad especialidad);
     int eliminar(int idEspecialidad);
