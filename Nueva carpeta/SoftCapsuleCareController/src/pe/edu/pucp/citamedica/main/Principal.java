4b51e1c9f82aeba36a6ce0ea5e62351fd1ccf063
FrankDarkShadow <gianfranco.mpc@gmail.com>
2024-09-23 21:58:13 -0500

    correcion-minima
    correccion main nombre

--- Nueva carpeta/SoftCapsuleCareController/src/pe/edu/pucp/citamedica/main/Principal.java
+++ Nueva carpeta/SoftCapsuleCareController/src/pe/edu/pucp/citamedica/main/Principal.java
@@ -12,7 +12,7 @@ public class Principal {
     
     
     
-    public static void Main(String args[]){
+    public static void main(String args[]){
         
         
         System.out.println("Clase de pruebas: ");
