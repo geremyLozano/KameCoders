6343013bb604efbb961ca5aa4e5f51a742abcb84
FrankDarkShadow <gianfranco.mpc@gmail.com>
2024-09-24 19:46:01 -0500

    cambio
    cambio

--- Nueva carpeta/SoftCapsuleCareController/src/pe/edu/pucp/citamedica/dao/AuxiliarDao.java
+++ Nueva carpeta/SoftCapsuleCareController/src/pe/edu/pucp/citamedica/dao/AuxiliarDao.java
@@ -2,7 +2,7 @@ package pe.edu.pucp.citamedica.dao;
 import java.util.ArrayList;
 import pe.edu.pucp.citamedica.clinica.model.Auxiliar;
 
-public interface AuxiliarDAO {
+public interface AuxiliarDAO{
     int insertar(Auxiliar auxiliar);
     int modificar(Auxiliar auxiliar);
     int eliminar(int idAuxiliar);
