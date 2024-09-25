518517c2e81aa837dc3c3dc279a39a597f693c7c
geremyLozano <157999676+geremyLozano@users.noreply.github.com>
2024-09-24 22:53:42 -0500

    Add files via upload

--- Script_Procedimientos.sql
+++ Script_Procedimientos.sql
@@ -9,6 +9,17 @@ DROP PROCEDURE IF EXISTS MEDICO_INSERTAR;
 DROP PROCEDURE IF EXISTS MEDICO_MODIFICAR;
 DROP PROCEDURE IF EXISTS MEDICO_ELIMINAR;
 DROP PROCEDURE IF EXISTS MEDICO_LISTAR;
+-- Procedimientos relacionados a auxiliar
+DROP PROCEDURE IF EXISTS AUXILIAR_INSERTAR;
+DROP PROCEDURE IF EXISTS AUXILIAR_MODIFICAR;
+DROP PROCEDURE IF EXISTS AUXILIAR_ELIMINAR;
+DROP PROCEDURE IF EXISTS AUXILIAR_LISTAR;
+-- Procedimientos relacionados a administrador
+DROP PROCEDURE IF EXISTS ADMIN_INSERTAR;
+DROP PROCEDURE IF EXISTS ADMIN_MODIFICAR;
+DROP PROCEDURE IF EXISTS ADMIN_ELIMINAR;
+DROP PROCEDURE IF EXISTS ADMIN_LISTAR;
+
 
 -- Creación de Procedimientos Almacenados
 DELIMITER $
@@ -221,9 +232,7 @@ CREATE PROCEDURE MEDICO_MODIFICAR (
 BEGIN
     DECLARE persona_id INT;
 
-    SELECT idpersona INTO persona_id
-    FROM Medico
-    WHERE idMedico = _idMedico;
+    SELECT idpersona INTO persona_id FROM Medico WHERE idMedico = _idMedico;
     
     IF persona_id IS NOT NULL THEN
         UPDATE Medico SET activo = _activo WHERE idpersona = persona_id;
@@ -240,11 +249,11 @@ DELIMITER ;
 DELIMITER $$
 
 CREATE PROCEDURE MEDICO_ELIMINAR (
-    IN _id_medico INT
+    IN _idMedico INT
 )
 BEGIN
     DECLARE persona_id INT;
-    SELECT idpersona INTO persona_id FROM Medico WHERE idMedico = _id_medico;
+    SELECT idpersona INTO persona_id FROM Medico WHERE idMedico = _idMedico;
     
     IF persona_id IS NOT NULL THEN
         DELETE FROM Medico WHERE idMedico = _id_medico;
@@ -278,6 +287,246 @@ BEGIN
         per.direccion,
         per.fechaNacimiento
     FROM Medico m
+    JOIN Persona per ON p.idpersona = per.idpersona; 
+END $$
+
+DELIMITER ;
+
+
+-----------------------------------------------------------------
+
+DELIMITER $$
+
+CREATE PROCEDURE AUXILIAR_INSERTAR (
+    IN _nombre VARCHAR(60),
+    IN _apellido VARCHAR(60),
+    IN _correoElectronico VARCHAR(45),
+    IN _numTelefono INT,
+    IN _direccion VARCHAR(100),
+    IN _fechaNacimiento DATE,
+    IN _genero CHAR(1),
+    IN _idComunicacion INT,
+    IN _idEspecialidad INT,
+    IN _activo TINYINT
+)
+BEGIN
+    DECLARE last_insert_id INT;
+
+    INSERT INTO Persona (
+        nombre, 
+        apellido, 
+        correoElectronico, 
+        numTelefono, 
+        direccion, 
+        fechaNacimiento,
+        genero,
+        idComunicacion
+    ) 
+    VALUES (
+        _nombre, 
+        _apellido, 
+        _correoElectronico, 
+        _numTelefono, 
+        _direccion, 
+        _fechaNacimiento,
+        _genero,
+        _idComunicacion
+    );
+    
+    SET last_insert_id = @@last_insert_id;
+    
+    INSERT INTO Auxiliar (
+        idpersona, 
+        idespecialidad, 
+	activo
+    ) 
+    VALUES (
+        last_insert_id, 
+        _idespecialidad,
+        _activo 
+    );
+    
+END $$
+
+DELIMITER ;
+
+
+DELIMITER $$
+
+CREATE PROCEDURE AUXILIAR_MODIFICAR (
+    IN _idAuxiliar INT,
+    IN _activo TINYINT
+)
+BEGIN
+    DECLARE persona_id INT;
+
+    SELECT idpersona INTO persona_id FROM Auxiliar WHERE idAuxiliar = _idAuxiliar;
+    
+    IF persona_id IS NOT NULL THEN
+        UPDATE Auxiliar SET activo = _activo WHERE idpersona = persona_id;
+    ELSE
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'El idPaciente no existe o no está vinculado a una persona';
+    END IF;
+    
+END $$
+
+DELIMITER ;
+
+
+DELIMITER $$
+
+CREATE PROCEDURE AUXILIAR_ELIMINAR (
+    IN _idAuxiliar INT
+)
+BEGIN
+    DECLARE persona_id INT;
+    SELECT idpersona INTO persona_id FROM Auxiliar WHERE idAuxiliar = _idAuxiliar;
+    
+    IF persona_id IS NOT NULL THEN
+        DELETE FROM Auxiliar WHERE idAuxiliar = _idAuxiliar;
+        DELETE FROM Persona WHERE idpersona = persona_id;
+    ELSE
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'El idPaciente no existe';
+    END IF;
+
+END $$
+
+DELIMITER ;
+
+
+DELIMITER $$
+
+CREATE PROCEDURE AUXILIAR_LISTAR()
+BEGIN
+    SELECT 
+        a.idAuxiliar,
+	a.activo,
+        per.idpersona,
+        per.nombre,
+        per.apellido,
+        per.correoElectronico,
+        per.numTelefono,
+        per.direccion,
+        per.fechaNacimiento
+    FROM Auxiliar a
+    JOIN Persona per ON p.idpersona = per.idpersona; 
+END $$
+
+DELIMITER ;
+
+--------------------------------------------------------------
+
+DELIMITER $$
+
+CREATE PROCEDURE ADMIN_INSERTAR (
+    IN _nombre VARCHAR(60),
+    IN _apellido VARCHAR(60),
+    IN _correoElectronico VARCHAR(45),
+    IN _numTelefono INT,
+    IN _direccion VARCHAR(100),
+    IN _fechaNacimiento DATE,
+    IN _genero CHAR(1),
+    IN _idComunicacion INT,
+)
+BEGIN
+    DECLARE last_insert_id INT;
+
+    INSERT INTO Persona (
+        nombre, 
+        apellido, 
+        correoElectronico, 
+        numTelefono, 
+        direccion, 
+        fechaNacimiento,
+        genero,
+        idComunicacion
+    ) 
+    VALUES (
+        _nombre, 
+        _apellido, 
+        _correoElectronico, 
+        _numTelefono, 
+        _direccion, 
+        _fechaNacimiento,
+        _genero,
+        _idComunicacion
+    );
+    
+    SET last_insert_id = @@last_insert_id;
+    
+    INSERT INTO Auxiliar (
+        idpersona, 
+    ) 
+    VALUES (
+        last_insert_id, 
+    );
+    
+END $$
+
+DELIMITER ;
+
+
+DELIMITER $$
+
+CREATE PROCEDURE ADMIN_MODIFICAR (
+    IN _idAdmin INT,
+    IN _numTelefono TINYINT
+)
+BEGIN
+    DECLARE persona_id INT;
+
+    SELECT idpersona INTO persona_id FROM Administrador WHERE idAdmin = _idAdmin;
+    
+    IF persona_id IS NOT NULL THEN
+        UPDATE Persona SET numTelefono = _numTelefono WHERE idpersona = persona_id;
+    ELSE
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'El idPaciente no existe o no está vinculado a una persona';
+    END IF;
+    
+END $$
+
+DELIMITER ;
+
+
+DELIMITER $$
+
+CREATE PROCEDURE ADMIN_ELIMINAR (
+    IN _idAmin INT
+)
+BEGIN
+    DECLARE persona_id INT;
+    SELECT idpersona INTO persona_id FROM Administrador WHERE idAmin = _idAmin;
+    
+    IF persona_id IS NOT NULL THEN
+        DELETE FROM Administrador WHERE idAmin = _idAmin;
+        DELETE FROM Persona WHERE idpersona = persona_id;
+    ELSE
+        SIGNAL SQLSTATE '45000'
+        SET MESSAGE_TEXT = 'El idPaciente no existe';
+    END IF;
+
+END $$
+
+DELIMITER ;
+
+
+DELIMITER $$
+
+CREATE PROCEDURE ADMIN_LISTAR()
+BEGIN
+    SELECT 
+        ad.idAmin,
+        per.idpersona,
+        per.nombre,
+        per.apellido,
+        per.correoElectronico,
+        per.numTelefono,
+        per.direccion,
+        per.fechaNacimiento
+    FROM Administrador ad
     JOIN Persona per ON p.idpersona = per.idpersona; 
 END $$
 
