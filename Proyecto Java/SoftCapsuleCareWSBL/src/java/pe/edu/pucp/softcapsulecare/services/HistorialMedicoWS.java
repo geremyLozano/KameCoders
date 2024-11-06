61810a56b76d500c0e580234c9c84f1311cc1938
FrankDarkShadow <gianfranco.mpc@gmail.com>
2024-11-05 22:47:58 -0500

    agregoService

--- Proyecto Java/SoftCapsuleCareWSBL/src/java/pe/edu/pucp/softcapsulecare/services/HistorialMedicoWS.java
+++ Proyecto Java/SoftCapsuleCareWSBL/src/java/pe/edu/pucp/softcapsulecare/services/HistorialMedicoWS.java
@@ -7,11 +7,13 @@ package pe.edu.pucp.softcapsulecare.services;
 import jakarta.jws.WebService;
 import jakarta.jws.WebMethod;
 import jakarta.jws.WebParam;
+import java.util.ArrayList;
 
 
 
 import pe.edu.pucp.citamedica.dao.HistorialMedicoDAO;
 import pe.edu.pucp.citamedica.model.consultas.HistorialMedico;
+import pe.edu.pucp.citamedica.model.dto.HistorialMedicoDto;
 import pe.edu.pucp.citamedica.mysql.HistorialMedicoMySQL;
 
 
@@ -50,6 +52,63 @@ public class HistorialMedicoWS {
         
         return historial;
     }
+    
+    
+    
+    
+    @WebMethod(operationName = "listarTodosPorCampImp")
+    public ArrayList<HistorialMedicoDto> listarTodosPorCampImp() {
+        ArrayList<HistorialMedicoDto> historial = null;
+
+        try{
+            daoHistorial = new HistorialMedicoMySQL();
+            historial = daoHistorial.listarTodosPorCampImp();
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+
+        return historial;
+    }
+    
+    
+    
+    @WebMethod(operationName = "eliminarHistorialMedicoPorId")
+    public int eliminarHistorialMedicoPorId( int idHistorialMedico) {
+        
+        
+        daoHistorial = new HistorialMedicoMySQL();
+        
+        int resultado = daoHistorial.eliminar(idHistorialMedico);
+        
+        
+        return resultado;
+    }
+    
+    
+    
+    @WebMethod(operationName = "modificarHistorialMedico")
+    public int modificarHistorialMedico( HistorialMedico historial) {
+        
+        
+        
+        int resultado =0;
+
+        
+        
+        
+        try{
+            daoHistorial = new HistorialMedicoMySQL();
+            resultado = daoHistorial.modificar(historial);
+        }catch(Exception ex){
+            System.out.println(ex.getMessage());
+        }
+
+        return resultado;
+        
+
+        
+    }
+    
     
     
     
