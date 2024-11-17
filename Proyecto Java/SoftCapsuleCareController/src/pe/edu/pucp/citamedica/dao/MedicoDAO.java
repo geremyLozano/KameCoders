7a5da9d87f3ba816e6d0e0fe6d2e20cc559fc692
geremyLozano <157999676+geremyLozano@users.noreply.github.com>
2024-11-17 18:36:08 -0500

    Update MedicoDAO.java

--- Proyecto Java/SoftCapsuleCareController/src/pe/edu/pucp/citamedica/dao/MedicoDAO.java
+++ Proyecto Java/SoftCapsuleCareController/src/pe/edu/pucp/citamedica/dao/MedicoDAO.java
@@ -20,4 +20,5 @@ public interface MedicoDAO {
     ArrayList<Medico> listarTodos1();
     Medico obtenerPorId1(int idMedico);
     List<Medico> listarFiltro(String filtro);
+    int modificar_v2(Medico medico); 
 }
